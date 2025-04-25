2ab2eb453745d826b8e2f1bb88ee28086d58e4a2
Jatin Dudhani <jatindudhani07@gmail.com>
2025-04-25 18:39:17 +0530

    Update site with dropshipping simulation features similar to Shopify and WooCommerce

--- upload-to-github.bat
+++ upload-to-github.bat
@@ -3,11 +3,17 @@ echo Uploading changes to GitHub...
 
 cd /d "%~dp0"
 "C:\Program Files\Git\bin\git.exe" add .
-"C:\Program Files\Git\bin\git.exe" commit -m "Update site with new features"
+"C:\Program Files\Git\bin\git.exe" commit -m "Update site with dropshipping simulation features similar to Shopify and WooCommerce"
 "C:\Program Files\Git\bin\git.exe" push
 
 echo.
 echo If successful, your updated site is now on GitHub!
+echo The site now includes advanced dropshipping simulation features including:
+echo - Supplier management and product importing
+echo - Inventory syncing with suppliers
+echo - Order routing and fulfillment
+echo - Automated shipping calculations
+echo - Marketing tools like abandoned cart recovery
 echo.
 echo Press any key to exit
 pause 
